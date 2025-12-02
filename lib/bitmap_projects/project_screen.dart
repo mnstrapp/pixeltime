cf7bd7f2aed436160dd091ff0cd24e1c1762d287
Michael Christenson II <m3talsmith@gmail.com>
2025-12-02 08:19:38 -0500

    Scaffolding Layer Controls

--- lib/bitmap_projects/project_screen.dart
+++ lib/bitmap_projects/project_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../models/bitmap_project.dart';
+import '../ui/theme.dart';
+import 'ui/layers_widget.dart';
 
 class BitmapProjectScreen extends ConsumerStatefulWidget {
   final BitmapProject project;
@@ -9,10 +11,18 @@ class BitmapProjectScreen extends ConsumerStatefulWidget {
 
   @override
   ConsumerState<BitmapProjectScreen> createState() =>
-      _BitmapProjectScreenState();
+      BitmapProjectScreenState();
+
+  static BitmapProjectScreenState of(BuildContext context) {
+    final state = context.findAncestorStateOfType<BitmapProjectScreenState>();
+    if (state == null) {
+      throw Exception('BitmapProjectScreen not found in context');
+    }
+    return state;
+  }
 }
 
-class _BitmapProjectScreenState extends ConsumerState<BitmapProjectScreen> {
+class BitmapProjectScreenState extends ConsumerState<BitmapProjectScreen> {
   final _horizontalScrollController = ScrollController();
   final _verticalScrollController = ScrollController();
 
@@ -27,17 +37,27 @@ class _BitmapProjectScreenState extends ConsumerState<BitmapProjectScreen> {
   Widget build(BuildContext context) {
     final project = widget.project;
     final size = MediaQuery.sizeOf(context);
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Container(
-          width: size.width * 2,
-          height: size.height * 2,
-          color: Colors.white,
-          child: Center(child: Text(project.name)),
+    return Stack(
+      children: [
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              width: size.width,
+              height: size.height,
+              color: Colors.white,
+              child: Center(child: Text(project.name)),
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          right: BaseTheme.borderRadiusMedium,
+          top: 0,
+          bottom: 0,
+          child: Center(child: BitmapProjectLayersWidget()),
+        ),
+      ],
     );
   }
 }
